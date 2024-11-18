759ab41df0a8e2ffeaab148addde68f320f138e2
David Kwon <david.kwon@clover.com>
2024-11-18 14:13:17 -0700

    Add ArticleDetails component for detailed view

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { PagesList } from './PagesList'
+import ArticleDetails from './ArticleDetails'
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api'
