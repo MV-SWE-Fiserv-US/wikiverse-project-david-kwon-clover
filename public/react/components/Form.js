077d8beb7013a2fa490de079782ece089973701d
David Kwon <david.kwon@clover.com>
2024-11-19 09:53:44 -0700

    Link form inputs to state

--- public/react/components/Form.js
+++ public/react/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -6,20 +6,62 @@ const Form = () => {
     content: "",
     name: "",
     email: "",
-    tags: ""
-  })
+    tags: "",
+  });
 
-  
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((previousFormData) => ({ ...previousFormData, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className='add-article-form'>
-        <input type='text' name="title" placeholder='Article Title' required></input>
-        <input type='text' name="name" placeholder='Author Name' required></input>
-        <input type='text' name="email" placeholder='Author Email' required></input>
-        <textarea name="content" placeholder='Article Content' required></textarea>
-        <input type='text' name="tags" placeholder='Tags(separated by space)' required></input>
-        <button>Create Page</button>
+    <form className="add-article-form">
+      <input
+        type="text"
+        name="title"
+        placeholder="Article Title"
+        required
+        value={formData.title}
+        onChange={handleChange}
+      ></input>
+      <textarea
+        name="content"
+        placeholder="Article Content"
+        required
+        value={formData.content}
+        onChange={handleChange}
+      ></textarea>
+      <input
+        type="text"
+        name="name"
+        placeholder="Author Name"
+        required
+        value={formData.name}
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        name="email"
+        placeholder="Author Email"
+        required
+        value={formData.email}
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        name="tags"
+        placeholder="Tags(separated by space)"
+        required
+        value={formData.tags}
+        onChange={handleChange}
+      ></input>
+      <button className="form-submit-button">Create Page</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
