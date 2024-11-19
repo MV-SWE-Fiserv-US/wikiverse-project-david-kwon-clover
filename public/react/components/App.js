6de4b00d37508c60738273ca9be7d16443cb23c2
David Kwon <david.kwon@clover.com>
2024-11-19 08:35:03 -0700

    Move articleDetials state and fetch to ArticleDetailsComponent and pass slug prop

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -8,8 +8,6 @@ import apiURL from '../api'
 export const App = () => {
   const [pages, setPages] = useState([])
   const [slug, setSlug] = useState("");
-  const [articleDetails, setArticleDetails] = useState(null);
-  
   
   async function fetchPages() {
     try {
@@ -21,30 +19,15 @@ export const App = () => {
     }
   }
 
-  async function fetchArticleDetails(slug) {
-    try {
-      const response = await fetch(`${apiURL}/wiki/${slug}`)
-      const data = await response.json();    
-      setArticleDetails(data)
-    } catch (err) {
-      console.log('Oh no an error! ', err)
-    }
-  }
-
   useEffect(() => {
     fetchPages()
   }, [])
-
-  useEffect(() => {
-    fetchArticleDetails(slug);
-  }, [slug]);
-
 
   return (
 		<main className="landing-page">
       <h1>WikiVerse</h1>
 			<h2>An interesting 📚</h2>
-      {slug && articleDetails ? <ArticleDetails articleDetails={articleDetails} /> : <PagesList pages={pages} setSlug={setSlug} /> }
+      {slug ? <ArticleDetails slug={slug} /> : <PagesList pages={pages} setSlug={setSlug} /> }
 		</main>
   )
 }
--- public/react/components/ArticleDetails.js
+++ public/react/components/ArticleDetails.js
@@ -1,17 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import apiURL from '../api';
 
-const ArticleDetails = ({ articleDetails }) => {
+const ArticleDetails = ({ slug }) => {
+  const [articleDetails, setArticleDetails] = useState({
+    title: "Loading..",
+    author: {
+      name: "Loading.."
+    },
+    createdAt: "Loading..",
+    content: "Loading..",
+    tags: [
+      {
+        name: "Loading.."
+      }
+    ]
+  });
+
+  async function fetchArticleDetails(slug) {
+    try {
+      const response = await fetch(`${apiURL}/wiki/${slug}`)
+      const data = await response.json();    
+      setArticleDetails(data)
+    } catch (err) {
+      console.log('Oh no an error! ', err)
+    }
+  }
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  }
+
   useEffect(() => {
-    console.log(articleDetails);
+    fetchArticleDetails(slug);
   }, [])
+
   return (
-    <article>
-      {/* <h1>{articleDetails.title}</h1>
+    <article className='article-details'>
+      <h1 className='article-details-title'>{articleDetails.title}</h1>
       <h4>Author: {articleDetails.author.name}</h4>
-      <h4>Published: {articleDetails.createdAt.slice(0,10)}</h4>
-      <p>Content: {articleDetails.content}</p> */}
+      <h4>Published: {formatDate(articleDetails.createdAt.slice(0,10))}</h4>
+      <p>{articleDetails.content}</p>
+      <h5>Tags: </h5>
+      <ul>
+        {articleDetails.tags.map((tag) => {
+          return <li key={tag.id}>{tag.name}</li>
+        })}
+      </ul>
     </article>
   )
+  
 }
 
 export default ArticleDetails
